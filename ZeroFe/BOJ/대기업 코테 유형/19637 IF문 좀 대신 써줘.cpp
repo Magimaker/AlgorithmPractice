06711c5ef515630ab163d883842c5e4fe22ccff8
ZeroFe <skel2cha@gmail.com>
2022-11-17 11:53:21 +0900

    19637 IF 문 좀 대신 써줘

---
+++ ZeroFe/BOJ/대기업 코테 유형/19637 IF문 좀 대신 써줘.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int titleCount, userCount;
+    cin >> titleCount >> userCount;
+
+    multimap<int, string> titles;
+    for (int i = 0; i < titleCount; i++)
+    {
+        int power;
+        string title;
+        cin >> title >> power;
+    	titles.insert({ power, title });
+    }
+
+    for (int i = 0; i < userCount; i++)
+    {
+        int power;
+        cin >> power;
+        auto iter = titles.lower_bound(power);
+        cout << iter->second << '\n';
+    }
+
+    return 0;
+}
