841ae110061424216efe9be5fc32fc263252646f
ZeroFe <skel2cha@gmail.com>
2022-11-18 12:45:45 +0900

    백준 25757 임스와 함께하는 미니게임

---
+++ ZeroFe/BOJ/대기업 코테 유형/25757 임스와 함께하는 미니게임.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    char game;
+    int count;
+    cin >> count >> game;
+
+    string name;
+    auto set = unordered_set<string>();
+    for (int i = 0; i < count; i++)
+    {
+        cin >> name;
+        auto iter = set.find(name);
+        if (iter == set.end())
+        {
+            set.insert(name);
+        }
+    }
+
+    int div = 1;
+    switch (game)
+    {
+	    case 'Y': div = 1; break;
+	    case 'F': div = 2; break;
+	    case 'O': div = 3; break;
+    }
+
+    cout << set.size() / div;
+
+    return 0;
+}
