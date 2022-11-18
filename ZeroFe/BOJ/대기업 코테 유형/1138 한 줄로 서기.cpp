d4d0b1cb5fc6dc9085c904a81c1612288145ecdf
ZeroFe <skel2cha@gmail.com>
2022-11-18 10:46:27 +0900

    1138 한 줄로 서기

---
+++ ZeroFe/BOJ/대기업 코테 유형/1138 한 줄로 서기.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int personCount;
+    cin >> personCount;
+
+    int num;
+    vector<int> persons = vector<int>(personCount);
+    for (int p = 1; p <= personCount; p++)
+    {
+        cin >> num;
+        int idx = 0;
+        for (idx = 0; idx < personCount; ++idx)
+        {
+            if (persons[idx] == 0)
+            {
+                if (num == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    num--;
+                }
+            }
+        }
+
+        persons[idx] = p;
+    }
+
+    // 출력
+    cout << persons[0];
+    for (int i = 1; i < personCount; i++)
+    {
+        cout << " " << persons[i];
+    }
+
+    return 0;
+}
