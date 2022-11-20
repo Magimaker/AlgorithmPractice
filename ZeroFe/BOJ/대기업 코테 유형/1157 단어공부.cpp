32433dfc5c69b1ca6c6d769d8c31ee7d0508d74e
ZeroFe <skel2cha@gmail.com>
2022-11-20 11:16:14 +0900

    백준 1157, 2075

---
+++ ZeroFe/BOJ/대기업 코테 유형/1157 단어공부.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int alphabets[26];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    string word;
+    cin >> word;
+
+    if (word.empty())
+    {
+        cout << "?";
+        return 0;
+    }
+
+    for (auto c : word)
+    {
+        auto bigC = toupper(c);
+        alphabets[bigC - 'A']++;
+    }
+
+    int maxCount = 0;
+    char maxAlpha;
+    for (int i = 0; i < 26; i++)
+    {
+	    if (alphabets[i] > maxCount)
+	    {
+            maxCount = alphabets[i];
+            maxAlpha = static_cast<char>('A' + i);
+	    }
+        else if (alphabets[i] == maxCount)
+        {
+            maxAlpha = '?';
+        }
+    }
+
+    cout << maxAlpha;
+
+    return 0;
+}
---
+++ ZeroFe/BOJ/대기업 코테 유형/2075 N번째 큰 수.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int VALUE_MIN = -1000000001;
+
+int alphabets[26];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    auto table = vector<vector<int>>(n, vector<int>(n));
+    for (int y = 0; y < n; ++y)
+    {
+	    for (int x = 0; x < n; ++x)
+	    {
+            cin >> table[y][x];
+	    }
+    }
+
+    int rank = 0;
+    int current = 0;
+    auto orders = vector<int>(n, n - 1);
+    while (rank < n)
+    {
+        int maxNum = VALUE_MIN;
+        int maxIdx = 0;
+	    for (int i = 0; i < n; ++i)
+	    {
+            int num = table[orders[i]][i];
+            if (num > maxNum)
+		    {
+                maxNum = num;
+                maxIdx = i;
+		    }
+	    }
+        current = maxNum;
+        rank++;
+        orders[maxIdx]--;
+    }
+
+    cout << current;
+
+    return 0;
+}
