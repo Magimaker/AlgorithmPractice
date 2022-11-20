f3aed4d5e42b3f4181d9229781480a4b1618c617
ZeroFe <skel2cha@gmail.com>
2022-11-20 12:48:15 +0900

    백준 23971 ZOAC4

---
+++ ZeroFe/BOJ/대기업 코테 유형/23971 ZOAC 4.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int rowNum, colNum, divRow, divCol;
+    cin >> rowNum >> colNum >> divRow >> divCol;
+
+    // 간격 + 사람 자리 칸 수
+	divRow++;
+    divCol++;
+
+    // 간격을 div로 나눌 때 몫과 나머지
+    int rowQuot, colQuot, rowRem, colRem;
+    rowQuot = rowNum / divRow;
+    colQuot = colNum / divCol;
+    rowRem = rowNum % divRow;
+    colRem = colNum % divCol;
+
+    int resRow = rowQuot + (rowRem != 0 ? 1 : 0);
+    int resCol = colQuot + (colRem != 0 ? 1 : 0);
+
+    cout << resRow * resCol;
+
+    return 0;
+}
