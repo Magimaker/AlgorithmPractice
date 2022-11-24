27f34d9614c68af6b0bf7c46e5e361f82568d1d6
ZeroFe <skel2cha@gmail.com>
2022-11-24 09:54:30 +0900

    백준 1927 최소 힙

---
+++ ZeroFe/BOJ/대기업 코테 유형/1927 최소힙.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    priority_queue<int, vector<int>, std::greater<>> pq;
+    int input;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> input;
+	    if (input != 0)
+	    {
+            pq.push(input);
+            continue;
+	    }
+
+        if (pq.empty())
+        {
+            cout << "0" << '\n';
+        }
+        else
+        {
+            int minValue = pq.top();
+            pq.pop();
+            cout << minValue << '\n';
+        }
+    }
+
+    return 0;
+}
