59fc965037c1a30232ec2170b626a39490c1826d
ZeroFe <skel2cha@gmail.com>
2022-11-24 11:44:25 +0900

    백준 2512 예산 백준 13144 List of Unique Number

---
+++ ZeroFe/BOJ/대기업 코테 유형/13144 List of Unique Number.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+// 수열에 저장된 값
+int nums[100001];
+// 수열의 중복 개수 확인하는 용도
+int memo[100001];
+
+long long GetCases(int n)
+{
+    assert(n >= 0);
+
+	if (n == 0)
+	{
+        return 0;
+	}
+
+	auto ll = static_cast<long long>(n);
+    return (ll * (ll + 1)) / 2;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> nums[i];
+    }
+
+    long long total = 0;
+    int end = 0;
+    int prevEnd = 0;
+    for (int curr = 0; curr < n; ++curr)
+    {
+        // 처음 탐색
+	    while (end < n && memo[nums[end]] == 0)
+	    {
+            memo[nums[end]]++;
+            end++;
+	    }
+
+        total += GetCases(end - curr) - GetCases(prevEnd - curr);
+
+        prevEnd = end;
+        memo[nums[curr]]--;
+    }
+
+    cout << total;
+
+    return 0;
+}
---
+++ ZeroFe/BOJ/대기업 코테 유형/2512 예산.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, total;
+    cin >> n;
+
+    auto cities = vector<int>(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> cities[i];
+    }
+    cin >> total;
+
+    sort(cities.begin(), cities.end());
+
+    int curr = 0;
+    int avg;
+    while (curr < n)
+    {
+        int prev = curr;
+        avg = total / (n - curr);
+
+        while (curr < n && cities[curr] <= avg)
+        {
+            total -= cities[curr++];
+        }
+
+	    if (prev == curr)
+	    {
+            break;
+	    }
+    }
+
+    avg = min(avg, cities[n - 1]);
+
+    cout << avg;
+
+    return 0;
+}
