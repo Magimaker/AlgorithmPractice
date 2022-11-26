8ff1a4dfc23aac6e93fe8f640755a3d3080fe6fe
ZeroFe <skel2cha@gmail.com>
2022-11-26 10:42:45 +0900

    백준 2467 용액

---
+++ ZeroFe/BOJ/대기업 코테 유형/2467 용액.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<int> nums = vector<int>(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> nums[i];
+    }
+
+    int minValue = INT_MAX;
+    int ans1, ans2;
+    int start = 0;
+	int end = n - 1;
+    while (start < end)
+    {
+        int val = nums[start] + nums[end];
+        int absVal = abs(val);
+	    if (absVal <= minValue)
+	    {
+            minValue = absVal;
+            ans1 = nums[start];
+            ans2 = nums[end];
+	    }
+
+        if (val < 0)
+        {
+            start++;
+        }
+        else
+        {
+            end--;
+        }
+    }
+
+    cout << ans1 << " " << ans2;
+
+    return 0;
+}
