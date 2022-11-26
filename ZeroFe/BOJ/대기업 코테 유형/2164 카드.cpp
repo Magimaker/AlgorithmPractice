e0396702e0b7d9e891642eb10daf516b972e0e36
ZeroFe <skel2cha@gmail.com>
2022-11-26 11:37:54 +0900

    백준 2164 카드

---
+++ ZeroFe/BOJ/대기업 코테 유형/2164 카드.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    if (n == 1)
+    {
+        cout << 1;
+        return 0;
+    }
+
+    int last = 2;
+    int multiple = 1;
+    while (n > 2)
+    {
+        int isOdd = n % 2;
+        n = (n >> 1) + isOdd;
+        multiple <<= 1;
+        if (!isOdd)
+        {
+            last += multiple;
+        }
+    }
+
+    cout << last;
+
+    return 0;
+}
