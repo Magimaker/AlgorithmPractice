837d1312bb142b4f067f6eba58b7b794a8e9e671
ZeroFe <skel2cha@gmail.com>
2022-11-27 12:13:16 +0900

    백준 실버3 9461 파도반 수열

---
+++ ZeroFe/BOJ/4S_3/9461 파도반 수열.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+long long dp[101];
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    // 먼저 파도반 수열 구하기
+    dp[0] = dp[1] = dp[2] = 1;
+    for (int i = 3; i < 100; ++i)
+    {
+        dp[i] = dp[i - 2] + dp[i - 3];
+    }
+
+    int T;
+    cin >> T;
+
+    for (int t = 0; t < T; ++t)
+    {
+        int n;
+        cin >> n;
+        cout << dp[n-1] << '\n';
+    }
+
+    return 0;
+}
