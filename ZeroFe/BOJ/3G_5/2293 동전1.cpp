2bbcb1a8b9d896c9ae21a409b2f8ca8e8bfc5f01
ZeroFe <skel2cha@gmail.com>
2022-11-30 01:13:23 +0900

    백준 2293 동전1

---
+++ ZeroFe/BOJ/3G_5/2293 동전1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int dp[100001];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, k;
+    cin >> n >> k;
+
+    dp[0] = 1;
+    vector<int> coins = vector<int>(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> coins[i];
+    }
+
+    for (int coin : coins)
+    {
+	    for (int i = coin; i <= k; ++i)
+	    {
+            dp[i] += dp[i - coin];
+	    }
+    }
+
+    cout << dp[k];
+    
+    return 0;
+}
