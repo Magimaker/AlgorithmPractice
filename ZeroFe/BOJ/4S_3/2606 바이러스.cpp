14717c701991fcb2fb61d60f31106e9c0926bc11
ZeroFe <skel2cha@gmail.com>
2022-12-02 10:13:30 +0900

    백준 실버3 바이러스

---
+++ ZeroFe/BOJ/4S_3/2606 바이러스.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> connected;
+vector<bool> visited;
+int infected = 0;
+
+void DFS(int curr)
+{
+	if (visited[curr])
+	{
+        return;
+	}
+
+    visited[curr] = true;
+    infected++;
+
+	for (int idx : connected[curr])
+	{
+        DFS(idx);
+	}
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, c;
+    cin >> n >> c;
+
+     visited = vector<bool>(n + 1, false);
+     connected = vector<vector<int>>(n + 1, vector<int>());
+
+    for (int i = 0; i < c; ++i)
+    {
+        int start, end;
+        cin >> start >> end;
+
+        connected[start].push_back(end);
+        connected[end].push_back(start);
+    }
+
+    DFS(1);
+
+    // 1번 컴퓨터 제외
+    cout << infected - 1;
+    
+    return 0;
+}
