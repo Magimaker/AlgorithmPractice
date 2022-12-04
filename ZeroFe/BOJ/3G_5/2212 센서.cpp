793d085f04ecec761ba5662a0af2d15986919eb2
ZeroFe <skel2cha@gmail.com>
2022-12-04 13:21:37 +0900

    백준 골드5 센서

---
+++ ZeroFe/BOJ/3G_5/2212 센서.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N, K;
+    cin >> N >> K;
+
+    auto sensors = vector<int>(N);
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> sensors[i];
+    }
+    sort(sensors.begin(), sensors.end());
+
+    auto diffs = vector<int>(N - 1);
+    for (int i = 0; i < N - 1; ++i)
+    {
+        diffs[i] = sensors[i + 1] - sensors[i];
+    }
+    sort(diffs.begin(), diffs.end());
+
+    int ans = 0;
+    for (int i = 0; i < N - K; ++i)
+    {
+        ans += diffs[i];
+    }
+    cout << ans;
+    
+    return 0;
+}
