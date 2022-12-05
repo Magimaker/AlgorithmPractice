97604d9c3f68bc8b4c6e24c91762bb1d3a59efb7
ZeroFe <skel2cha@gmail.com>
2022-12-05 10:56:29 +0900

    백준 골드5 스카이라인 쉬운거

---
+++ ZeroFe/BOJ/3G_5/1863 스카이라인 쉬운거.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N;
+    cin >> N;
+
+    stack<int> buildings;
+    int total = 0;
+    for (int i = 0; i < N; ++i)
+    {
+        int x, height;
+        cin >> x >> height;
+
+        while (!buildings.empty() && height < buildings.top())
+        {
+            buildings.pop();
+            total++;
+        }
+        if (height > 0)
+        {
+	        if (buildings.empty() || height > buildings.top())
+	        {
+                buildings.push(height);
+	        }
+        }
+    }
+
+    // 스택에 남아있는거 전부 비우기
+    total += buildings.size();
+
+    cout << total;
+
+    return 0;
+}
