d2e46e36fe5fbed96675ba7053789705877947e7
ZeroFe <skel2cha@gmail.com>
2022-12-06 15:38:38 +0900

    백준 실버1 겹치는 건 싫어, 실버3 블로그

---
+++ ZeroFe/BOJ/4S_1/20922 겹치는 건 싫어.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int numCounts[100001];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N, K;
+    cin >> N >> K;
+
+    int maxLength = 0;
+    queue<int> q;
+    for (int i = 0; i < N; ++i)
+    {
+        int num;
+        cin >> num;
+        q.push(num);
+        numCounts[num]++;
+        while (numCounts[num] > K)
+        {
+            int f = q.front();
+            q.pop();
+            numCounts[f]--;
+        }
+        maxLength = max(maxLength, static_cast<int>(q.size()));
+    }
+
+    cout << maxLength;
+
+    return 0;
+}
---
+++ ZeroFe/BOJ/4S_3/21921 블로그.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N, X;
+    cin >> N >> X;
+
+    vector<int> days(N);
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> days[i];
+    }
+
+    int currVisitor = 0;
+    int maxVisitor = 0;
+    int times = 1;
+    // 1일차부터 X일차까지 방문자 수
+    for (int i = 0; i < X; ++i)
+    {
+        currVisitor += days[i];
+    }
+    maxVisitor = currVisitor;
+
+    for (int i = X; i < N; ++i)
+    {
+        currVisitor -= days[i - X];
+        currVisitor += days[i];
+        if (currVisitor > maxVisitor)
+        {
+            maxVisitor = currVisitor;
+            times = 1;
+        }
+        else if (currVisitor == maxVisitor)
+        {
+            times++;
+        }
+    }
+
+    if (maxVisitor == 0)
+    {
+        cout << "SAD";
+    }
+    else
+    {
+        cout << maxVisitor << '\n' << times;
+    }
+
+    return 0;
+}
