768c279eea12b892894e20816c5888cc848fc1f4
ZeroFe <skel2cha@gmail.com>
2022-12-11 10:27:12 +0900

    백준 대기업 성장 시리즈 실버2

---
+++ ZeroFe/BOJ/4S_2/1406 에디터.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    std::cout.tie(0);
+
+    string line;
+    cin >> line;
+    std::list<char> text;
+    for (char c : line)
+    {
+        text.push_back(c);
+    }
+    auto cursor = text.end();
+
+    int N;
+    cin >> N;
+    char command;
+    char c;
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> command;
+        switch (command)
+        {
+        case 'L':
+	        if (cursor != text.begin())
+	        {
+                --cursor;
+	        }
+            break;
+        case 'D':
+            if (cursor != text.end())
+            {
+                ++cursor;
+            }
+            break;
+        case 'B':
+	        if (cursor != text.begin())
+	        {
+                auto temp = cursor;
+                --temp;
+                text.erase(temp);
+	        }
+            break;
+        case 'P':
+            cin >> c;
+            text.insert(cursor, c);
+            break;
+        }
+    }
+
+    for (char c : text)
+    {
+        cout << c;
+    }
+
+    return 0;
+}
---
+++ ZeroFe/BOJ/4S_2/2304 창고 다각형.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    std::cout.tie(0);
+
+    int N;
+    cin >> N;
+
+    int end = 0;
+    vector<pair<int, int>> inputs(N);
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> inputs[i].first >> inputs[i].second;
+        end = max(end, inputs[i].first + 1);
+    }
+
+    vector<int> heights(end);
+    for (const auto& input : inputs)
+    {
+        heights[input.first] = input.second;
+    }
+    
+    int left = 0;
+    int right = end - 1;
+    int leftHeight = heights[left];
+    int rightHeight = heights[right];
+    int sum = 0;
+    while (left <= right)
+    {
+	    if (leftHeight <= rightHeight)
+	    {
+            sum += leftHeight;
+            leftHeight = max(leftHeight, heights[++left]);
+	    }
+        else
+        {
+            sum += rightHeight;
+            rightHeight = max(rightHeight, heights[--right]);
+        }
+    }
+
+    cout << sum;
+
+    return 0;
+}
