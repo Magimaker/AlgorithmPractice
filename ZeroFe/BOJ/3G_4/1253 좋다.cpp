6055511d4b7fd8b93cad268e55fb10a7a77c070b
ZeroFe <skel2cha@gmail.com>
2022-12-14 14:53:20 +0900

    백준 골드4 좋다

---
+++ ZeroFe/BOJ/3G_4/1253 좋다.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    std::cout.tie(0);
+
+    int N;
+    cin >> N;
+    vector<int> nums(N);
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> nums[i];
+    }
+    sort(nums.begin(), nums.end());
+
+    int count = 0;
+    for (int i = 0; i < nums.size(); ++i)
+    {
+        int target = nums[i];
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left < right)
+        {
+            int sum = nums[left] + nums[right];
+            if (sum == target)
+            {
+                if (left != i && right != i) {
+                    count++;
+                    break;
+                }
+                else if (left == i) left++;
+                else if (right == i) right--;
+            }
+            else if (sum < target)
+            {
+                left++;
+            }
+            else if (sum > target)
+            {
+                right--;
+            }
+        }
+    }
+
+    cout << count;
+
+    return 0;
+}
