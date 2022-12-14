b6306ca6db4c291a01e2daab6d12aa2788874c99
ZeroFe <skel2cha@gmail.com>
2022-12-14 15:10:11 +0900

    백준 골드3 파티(플루이드 알고리즘으로 풂)

---
+++ ZeroFe/BOJ/3G_3/1238 파티.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+const int INF = 1000000000;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    std::cout.tie(0);
+
+    int N, M, X;
+    cin >> N >> M >> X;
+
+    // Floyd
+    // 초기화
+    auto paths = vector<vector<int>>(N, vector<int>(N, INF));
+    for (int i = 0; i < N; ++i)
+    {
+        paths[i][i] = 0;
+    }
+
+    for (int i = 0; i < M; ++i)
+    {
+        int start, end, distance;
+        cin >> start >> end >> distance;
+        paths[start - 1][end - 1] = distance;
+    }
+
+    for (int k = 0; k < N; ++k)
+    {
+	    for (int i = 0; i < N; ++i)
+	    {
+		    for (int j = 0; j < N; ++j)
+		    {
+                paths[i][j] = min(paths[i][j], paths[i][k] + paths[k][j]);
+		    }
+	    }
+    }
+
+    // 각 학생 별로 최다시간 구하기
+    int maxVal = 0;
+    for (int i = 0; i < N; ++i)
+    {
+        maxVal = max(maxVal, paths[i][X - 1] + paths[X - 1][i]);
+    }
+
+    cout << maxVal;
+
+    return 0;
+}
