5a710c2a791cff83cc299fe7c146733b28755487
ZeroFe <skel2cha@gmail.com>
2022-12-15 13:21:45 +0900

    백준 골드5 숨바꼭질 3 골드4 알파벳

---
+++ ZeroFe/BOJ/3G_4/1987 알파벳.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int MAX_SIZE = 20;
+
+char map[MAX_SIZE][MAX_SIZE];
+bool visited[26];
+
+int R, C;
+
+const int dy[] = {1, -1, 0, 0};
+const int dx[] = {0, 0, -1, 1};
+
+int Travel(int y, int x, int depth)
+{
+    int maxLength = depth;
+    for (int d = 0; d < 4; ++d)
+    {
+        int newY = y + dy[d];
+        int newX = x + dx[d];
+
+        if (newY < 0 || newY >= R || newX < 0 || newX >= C || map[newY][newX] == '.' || visited[map[newY][newX] - 'A'])
+        {
+            continue;
+        }
+
+        char backup = map[newY][newX];
+        map[newY][newX] = '.';
+        visited[backup - 'A'] = true;
+        maxLength = max(maxLength, Travel(newY, newX, depth + 1));
+        map[newY][newX] = backup;
+        visited[backup - 'A'] = false;
+    }
+
+    return maxLength;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    std::cout.tie(0);
+
+    cin >> R >> C;
+
+    string line;
+    for (int y = 0; y < R; ++y)
+    {
+        cin >> line;
+        for (int x = 0; x < C; ++x)
+        {
+            map[y][x] = line[x];
+        }
+    }
+
+    visited[map[0][0] - 'A'] = true;
+    map[0][0] = '.';
+    cout << Travel(0, 0, 1);
+    
+    return 0;
+}
---
+++ ZeroFe/BOJ/3G_5/13459 숨바꼭질 3.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+const int MAX_VAL = 100001;
+
+int N, K;
+vector<bool> visited(MAX_VAL);
+
+int Find()
+{
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+    pq.push({0, N});
+    visited[N] = true;
+
+    while (!pq.empty())
+    {
+        int time = pq.top().first;
+        int x = pq.top().second;
+        pq.pop();
+
+        if (x == K) return time;
+
+        int teleport = x << 1;
+        if (teleport < MAX_VAL && !visited[teleport]) {
+            pq.push({time, teleport});
+            visited[teleport] = true;
+        }
+
+        int up = x + 1;
+        if (up < MAX_VAL && !visited[up]) {
+            pq.push({ time + 1, up });
+            visited[up] = true;
+        }
+
+        int down = x - 1;
+        if (down >= 0 && !visited[down]) {
+            pq.push({ time + 1 , down });
+            visited[down] = true;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    std::cout.tie(0);
+
+    cin >> N >> K;
+
+    cout << Find();
+
+    return 0;
+}
