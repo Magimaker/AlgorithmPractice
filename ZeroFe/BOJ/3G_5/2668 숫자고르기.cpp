839b86a1b02a3e98f15a79cc484d0761d9ef8c22
ZeroFe <skel2cha@gmail.com>
2022-12-16 13:23:31 +0900

    백준 골드5 숫자고르기(방법 확인하고 풀어서 다시 풀어야 함)

---
+++ ZeroFe/BOJ/3G_5/2668 숫자고르기.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int N;
+int nums[101];
+bool visited[101];
+
+int result[101];
+int resultIdx = 0;
+
+void DFS(int current, int start)
+{
+	if (visited[current])
+	{
+        if (current == start)
+            result[resultIdx++] = start;
+	}
+    else
+    {
+        visited[current] = true;
+        DFS(nums[current], start);
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    std::cout.tie(0);
+    
+    cin >> N;
+
+    for (int i = 1; i <= N; ++i)
+    {
+        cin >> nums[i];
+    }
+
+    for (int i = 1; i <= N; ++i)
+    {
+        memset(visited, false, sizeof(visited));
+        DFS(i, i);
+    }
+
+    cout << resultIdx << '\n';
+    for (int i = 0; i < resultIdx; ++i)
+    {
+        cout << result[i] << '\n';
+    }
+    
+    return 0;
+}
