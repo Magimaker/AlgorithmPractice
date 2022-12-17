3b5922930076f6828123ef2decad284b413b7d3c
ZeroFe <skel2cha@gmail.com>
2022-12-17 14:35:05 +0900

    백준 실버5 줄세우기(버블소트)

---
+++ ZeroFe/BOJ/4S_5/10431 줄세우기.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    std::cout.tie(0);
+
+    int T;
+    cin >> T;
+    int arr[20] = {};
+    for (int tt = 1; tt <= T; ++tt)
+    {
+        int N;
+        cin >> N;
+
+        for (int i = 0; i < 20; ++i)
+        {
+            cin >> arr[i];
+        }
+
+        int count = 0;
+        for (int i = 0; i < 20; ++i)
+        {
+	        for (int j = 0; j < i; ++j)
+	        {
+		        if (arr[i] < arr[j])
+		        {
+                    swap(arr[i], arr[j]);
+                    count++;
+		        }
+	        }
+        }
+
+        cout << tt << " " << count << '\n';
+    }
+
+    return 0;
+}
