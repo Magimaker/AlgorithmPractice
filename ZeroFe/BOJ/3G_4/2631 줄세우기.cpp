43711c92d2f01d7b1eb8396b5fb86f3802ccb2c7
ZeroFe <skel2cha@gmail.com>
2022-12-25 09:40:48 +0900

    백준 골드4 줄세우기(DP)

---
+++ ZeroFe/BOJ/3G_4/2631 줄세우기.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N;
+    cin >> N;
+
+    vector<int> nums(N);
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> nums[i];
+    }
+
+    // 가장 큰 부분 증가 수열로 배치된 아이들을 기준으로 옮겨야 이동 수가 최소가 된다
+    // 고로, 전체 크기에서 부분 증가 수열의 크기만큼 빼면 된다
+    vector<int> orders;
+    for (int num : nums)
+    {
+        auto iter = upper_bound(orders.begin(), orders.end(), num);
+        if (iter == orders.end())
+        {
+            orders.push_back(num);
+        }
+        else
+        {
+            *iter = num;
+        }
+    }
+
+    cout << N - orders.size();
+
+    return 0;
+}
